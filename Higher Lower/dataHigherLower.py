ebb2486a7ffbaa6d1d8e227002e5661454075503
Muhammad Rafly Ash Shiddiqi <einrafh@gmail.com>
2023-07-18 23:08:02 +0700

    Higher Lower

---
+++ Higher Lower/dataHigherLower.py
@@ -0,0 +1,302 @@
+data = [
+    {
+        'nama': 'Instagram',
+        'jumlahFollower': 346,
+        'deskripsi': 'Platform media sosial',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Cristiano Ronaldo',
+        'jumlahFollower': 215,
+        'deskripsi': 'Pemain sepak bola',
+        'negara': 'Portugal'
+    },
+    {
+        'nama': 'Ariana Grande',
+        'jumlahFollower': 183,
+        'deskripsi': 'Musisi dan aktris',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Dwayne Johnson',
+        'jumlahFollower': 181,
+        'deskripsi': 'Aktor dan pegulat profesional',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Selena Gomez',
+        'jumlahFollower': 174,
+        'deskripsi': 'Musisi dan aktris',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Kylie Jenner',
+        'jumlahFollower': 172,
+        'deskripsi': 'Reality TV personality, pengusaha, dan Self-Made Miliarder',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Kim Kardashian',
+        'jumlahFollower': 167,
+        'deskripsi': 'Reality TV personality dan pengusaha',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Lionel Messi',
+        'jumlahFollower': 149,
+        'deskripsi': 'Pemain sepak bola',
+        'negara': 'Argentina'
+    },
+    {
+        'nama': 'Beyoncé',
+        'jumlahFollower': 145,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Neymar',
+        'jumlahFollower': 138,
+        'deskripsi': 'Pemain sepak bola',
+        'negara': 'Brasil'
+    },
+    {
+        'nama': 'National Geographic',
+        'jumlahFollower': 135,
+        'deskripsi': 'Majalah',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Justin Bieber',
+        'jumlahFollower': 133,
+        'deskripsi': 'Musisi',
+        'negara': 'Canada'
+    },
+    {
+        'nama': 'Taylor Swift',
+        'jumlahFollower': 131,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Kendall Jenner',
+        'jumlahFollower': 127,
+        'deskripsi': 'Reality TV personality dan Model',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Jennifer Lopez',
+        'jumlahFollower': 119,
+        'deskripsi': 'Musisi dan aktris',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Nicki Minaj',
+        'jumlahFollower': 113,
+        'deskripsi': 'Musisi',
+        'negara': 'Trinidad and Tobago'
+    },
+    {
+        'nama': 'Nike',
+        'jumlahFollower': 109,
+        'deskripsi': 'Sportswear multinational',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Khloé Kardashian',
+        'jumlahFollower': 108,
+        'deskripsi': 'Reality TV personality dan pengusaha',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Miley Cyrus',
+        'jumlahFollower': 107,
+        'deskripsi': 'Musisi dan aktris',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Katy Perry',
+        'jumlahFollower': 94,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Kourtney Kardashian',
+        'jumlahFollower': 90,
+        'deskripsi': 'Reality TV personality',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Kevin Hart',
+        'jumlahFollower': 89,
+        'deskripsi': 'Komedian dan aktor',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Ellen DeGeneres',
+        'jumlahFollower': 87,
+        'deskripsi': 'Komedian',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Real Madrid CF',
+        'jumlahFollower': 86,
+        'deskripsi': 'Klub sepak bola',
+        'negara': 'Spain'
+    },
+    {
+        'nama': 'FC Barcelona',
+        'jumlahFollower': 85,
+        'deskripsi': 'Klub sepak bola',
+        'negara': 'Spain'
+    },
+    {
+        'nama': 'Rihanna',
+        'jumlahFollower': 81,
+        'deskripsi': 'Musisi dan pengusaha',
+        'negara': 'Barbados'
+    },
+    {
+        'nama': 'Demi Lovato',
+        'jumlahFollower': 80,
+        'deskripsi': 'Musisi dan aktris',
+        'negara': 'United States'
+    },
+    {
+        'nama': "Victoria's Secret",
+        'jumlahFollower': 69,
+        'deskripsi': 'Merek pakaian dalam dan parfum',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Zendaya',
+        'jumlahFollower': 68,
+        'deskripsi': 'Aktris dan musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Shakira',
+        'jumlahFollower': 66,
+        'deskripsi': 'Musisi',
+        'negara': 'Colombia'
+    },
+    {
+        'nama': 'Drake',
+        'jumlahFollower': 65,
+        'deskripsi': 'Musisi',
+        'negara': 'Canada'
+    },
+    {
+        'nama': 'Chris Brown',
+        'jumlahFollower': 64,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'LeBron James',
+        'jumlahFollower': 63,
+        'deskripsi': 'Pemain basket',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Vin Diesel',
+        'jumlahFollower': 62,
+        'deskripsi': 'Aktor',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Cardi B',
+        'jumlahFollower': 67,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'David Beckham',
+        'jumlahFollower': 82,
+        'deskripsi': 'Pemain sepak bola',
+        'negara': 'United Kingdom'
+    },
+    {
+        'nama': 'Billie Eilish',
+        'jumlahFollower': 61,
+        'deskripsi': 'Musisi',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Justin Timberlake',
+        'jumlahFollower': 59,
+        'deskripsi': 'Musisi dan aktor',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'UEFA Champions League',
+        'jumlahFollower': 58,
+        'deskripsi': 'Kompetisi sepak bola klub',
+        'negara': 'Europe'
+    },
+    {
+        'nama': 'NASA',
+        'jumlahFollower': 56,
+        'deskripsi': 'Badan antariksa',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Emma Watson',
+        'jumlahFollower': 56,
+        'deskripsi': 'Aktris',
+        'negara': 'United Kingdom'
+    },
+    {
+        'nama': 'Shawn Mendes',
+        'jumlahFollower': 57,
+        'deskripsi': 'Musisi',
+        'negara': 'Canada'
+    },
+    {
+        'nama': 'Virat Kohli',
+        'jumlahFollower': 55,
+        'deskripsi': 'Pemain kriket',
+        'negara': 'India'
+    },
+    {
+        'nama': 'Gigi Hadid',
+        'jumlahFollower': 54,
+        'deskripsi': 'Model',
+        'negara': 'United States'
+    },
+    {
+        'nama': 'Priyanka Chopra Jonas',
+        'jumlahFollower': 53,
+        'deskripsi': 'Aktris dan musisi',
+        'negara': 'India'
+    },
+    {
+        'nama': '9GAG',
+        'jumlahFollower': 52,
+        'deskripsi': 'Platform media sosial',
+        'negara': 'China'
+    },
+    {
+        'nama': 'Ronaldinho',
+        'jumlahFollower': 51,
+        'deskripsi': 'Pemain sepak bola',
+        'negara': 'Brasil'
+    },
+    {
+        'nama': 'Maluma',
+        'jumlahFollower': 50,
+        'deskripsi': 'Musisi',
+        'negara': 'Colombia'
+    },
+    {
+        'nama': 'Camila Cabello',
+        'jumlahFollower': 49,
+        'deskripsi': 'Musisi',
+        'negara': 'Cuba'
+    },
+    {
+        'nama': 'NBA',
+        'jumlahFollower': 47,
+        'deskripsi': 'Kompetisi bola basket klub',
+        'negara': 'United States'
+    }
+]
---
+++ Higher Lower/main.py
@@ -0,0 +1,112 @@
+# Librari
+import asetHigherLower
+import dataHigherLower
+import random
+
+# Membuat fungsi clear terminal
+def clear():
+    print("\033[H\033[2J", end="", flush=True)
+
+# Membuat fungsi random akun untuk memilih data secara acak
+def randomAkun():
+    return random.choice(dataHigherLower.data)
+
+# Membuat fungsi format data
+def formatData(akun):
+    nama = akun["nama"]
+    deskripsi = akun["deskripsi"]
+    negara = akun["negara"]
+    return f"{nama}, seorang {deskripsi} dari {negara}"
+
+# Membuat fungsi untuk mengecek tebakan user benar atau salah
+def cekTebakan(tebakan, followerA, followerB):
+    if followerA > followerB:
+        return tebakan == "A"
+    elif followerA < followerB:
+        return tebakan == "B"
+
+# Membuat fungsi utama dari Higher Lower
+def main():
+    # Clear terminal
+    clear()
+
+    # Inisialisasi akun A dan B
+    akunA = randomAkun()
+    akunB = randomAkun()
+
+    # Inisialisasi skor
+    skor = 0
+
+    # Print logo Higher Lower
+    print(asetHigherLower.LOGO)
+
+    # Loop ketika user menjawab benar
+    while True:
+        # Mengganti nilai akun A menjadi akun B dan menginisialisasi akun B kembali
+        akunA = akunB
+        akunB = randomAkun()
+
+        # Mengecek jika nilai akun A dan B sama maka akun B akan di isi nilai yang lain dari list data
+        while akunA == akunB:
+            akunB = randomAkun()
+        
+        # Loop jika input tebakan tidak valid
+        tebakan = ""
+        while tebakan != "A" and tebakan != "B":
+            # Menampilkan komparasi nilai akun A terhadap akun B
+            print(f"Komparasi A: {formatData(akunA)}.")
+            print(asetHigherLower.VS)
+            print(f"Terhadap B: {formatData(akunB)}.")
+
+            # Input tebakan
+            print("Siapa yang memiliki lebih banyak follower di Instagram? Ketik \"A\" atau \"B\".")
+            tebakan = input("Jawab: ").upper()
+
+            # Mengecek input tebakan valid
+            if tebakan != "A" and tebakan != "B":
+                clear()
+                print(asetHigherLower.LOGO)
+                print("Input tidak valid.")
+                print(f"Skor kamu sekarang: {skor}")
+
+        # Inisialisasi jumlah follower akun A dan B
+        followerA = akunA["jumlahFollower"]
+        followerB = akunB["jumlahFollower"]
+
+        # Validasi tebakan user
+        validasi = cekTebakan(tebakan, followerA, followerB)
+
+        # Clear terminal
+        clear()
+
+        # Print logo Higher Lower
+        print(asetHigherLower.LOGO)
+
+        # Mengecek kondisi apakah tebakan user tepat atau tidak
+        if validasi:
+            skor += 1
+            print(f"Kamu benar! Skor kamu sekarang: {skor}")
+        else:
+            print(f"Maaf, kamu salah. Final skor kamu: {skor}")
+            break
+    
+    # Menanyakan apakah user ingin memainkan ulang permainan Higher Lower
+    while True:
+        print("Apakah kamu ingin memainkan Higher Lower lagi? Ketik \"ya\" atau \"tidak\".")
+        ulangi = input("Jawab: ").lower()
+
+        if ulangi == "ya":
+            main()
+        elif ulangi == "tidak":
+            return
+        else:
+            print("Input tidak valid.")
+            print("-----")
+
+# Memanggil fungsi main()
+main()
+
+# Penanda
+print("=" * 40)
+print("Caesar Cipher by Einrafh")
+print("=" * 40)
